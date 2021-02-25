bde5ad0f62d81e56698c3aed1e118e6bae7936f6
adhil284 <meous02@gmail.com>
2021-02-25 04:00:58 +0000

    comment

--- eventregistration/urls.py
+++ eventregistration/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from eventapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',views.home,name='home'),
+    path('register/',views.register, name='register'),
+    path('result/',views.result, name='result'),
 ]
+
